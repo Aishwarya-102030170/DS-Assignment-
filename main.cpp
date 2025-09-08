b98a6b5ecb0378076fabce38f918773798aeb8f8
Aishwarya-102030170 <averma4_be24@thapar.edu>
2025-09-08 14:50:25 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,79 @@
 #include <iostream>
-#include <string>
-#include <stack>
 using namespace std;
-void reverse(string s){
-    stack<char> st;
-    for(int i=0;i<s.length();i++){
-        st.push(s[i]);
+    // stack<int> st;
+    int msize=10;
+    int s[10];
+    int top=-1;
+void push(int ele){
+    if(top==msize){
+        cout<<"stack overloading"<<endl;
     }
-    string rev="";
-    for(int i=0;i<s.length();i++){
-      rev+=st.top();
-      st.pop();
+    else{
+        top=top+1;
+        s[top]=ele;
     }
-    cout<<"reversed string is: "<<rev;
+}
+void pop(){
+    if(top==-1){
+        cout<<"empty stack,unable to pop any ele"<<endl;
+    }
+    else{
+        top=top-1;
+    }
+}
+void topele(){
+     if(top==-1){
+        cout<<"empty stack,unable to find top ele"<<endl;
+    }
+    else{
+        cout<<"top = "<<s[top]<<endl;
+    }
+}
+void size(){
+    cout<<"current size = "<<top+1<<endl;
+}
+void isEmpty(){
+    if(top==-1){
+        cout<<"Stack is empty"<<endl;
+    }
+    else{
+        cout<<"Stack is not empty"<<endl;
+    }
+}
+void isFull(){
+    if(top==msize-1){
+        cout<<"Stack is full"<<endl;
+    }
+    else{
+        cout<<"Stack is not full"<<endl;
+    }
+}
+void peek(){
+     if(top==-1){
+        cout<<"empty stack,unable to find top ele"<<endl;
+    }
+    else{
+        cout<<"top = "<<s[top]<<endl;
+    }
+}
+void display(){
+    for(int i=0;i<=top;i++){
+        cout<<s[i]<<" ";
+    }
 }
 int main(){
-  string s;
-  cout<<"enter a string: ";
-  getline(cin,s);
-  reverse(s);
+    int ele1,ele2,ele3;
+    cout<<"Enter the elements to be added in stack: ";
+    cin>>ele1>>ele2>>ele3;
+    push(ele1);
+    push(ele2);
+    push(ele3);
+    pop();
+    topele();
+    size();
+    isEmpty();
+    isFull();
+    peek();
+    display();
+    return 0;
 }
